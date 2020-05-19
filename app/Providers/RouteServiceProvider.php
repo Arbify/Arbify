5b4aca09f5f7a2f5d0a514e460915de140b12fd2
Albert221 <w.albert221@gmail.com>
2020-05-19 21:45:09 +0200

    Move route model bindings to new method

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -6,34 +6,26 @@ use App\Contracts\Repositories\LanguageRepository;
 use App\Contracts\Repositories\MessageRepository;
 use App\Contracts\Repositories\ProjectRepository;
 use App\Contracts\Repositories\UserRepository;
-use App\Models\Language;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
     protected $namespace = 'App\Http\Controllers';
 
     /**
      * The path to the "home" route for your application.
-     *
-     * @var string
      */
     public const HOME = '/';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
     public function boot(): void
+    {
+        $this->bindModels();
+
+        parent::boot();
+    }
+
+    private function bindModels(): void
     {
         Route::bind('project', function ($value) {
             return app(ProjectRepository::class)->byId((int) $value);
@@ -54,8 +46,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::bind('user', function ($value) {
             return app(UserRepository::class)->byId((int) $value);
         });
-
-        parent::boot();
     }
 
     public function map(): void
@@ -64,14 +54,14 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapWebRoutes();
     }
 
-    protected function mapWebRoutes(): void
+    private function mapWebRoutes(): void
     {
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
     }
 
-    protected function mapApiRoutes(): void
+    private function mapApiRoutes(): void
     {
         Route::prefix('api')
             ->middleware('api')
