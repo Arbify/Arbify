8f043012b70d5f8b509fa7adcc567ce0c71e3ce0
Albert221 <w.albert221@gmail.com>
2020-05-22 01:37:28 +0200

    Remove alert about authorization WIP from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="container">
         <h2 class="mb-4">Dashboard</h2>
-        <p class="alert alert-info">Roles and access control is still a work in progress.</p>
         <p>Nothing to see here. Yet.</p>
     </div>
 @endsection
