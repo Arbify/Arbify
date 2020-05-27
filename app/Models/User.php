a2e5a5729d64db28975c18b43381d502100e9fe9
Albert221 <w.albert221@gmail.com>
2020-05-27 22:58:27 +0200

    Add Laravel Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
+    use HasApiTokens;
 
     public const ROLES = [
         self::ROLE_SUPER_ADMINISTRATOR,
