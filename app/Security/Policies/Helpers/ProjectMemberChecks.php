b635637b735b1f41eaf3a3f64f440091571c4d75
Albert221 <w.albert221@gmail.com>
2020-06-04 00:58:16 +0200

    Cache isLeadOrMemberInProject

--- app/Security/Policies/Helpers/ProjectMemberChecks.php
+++ app/Security/Policies/Helpers/ProjectMemberChecks.php
@@ -6,7 +6,9 @@ namespace Arbify\Security\Policies\Helpers;
 
 use Arbify\Contracts\Repositories\ProjectMemberRepository;
 use Arbify\Models\Project;
+use Arbify\Models\ProjectMember;
 use Arbify\Models\User;
+use Cache;
 
 trait ProjectMemberChecks
 {
@@ -23,16 +25,25 @@ trait ProjectMemberChecks
 
     private function isLeadInProject(User $user, Project $project): bool
     {
-        $member = $this->projectMemberRepository->byProjectAndUserOrNull($project, $user);
+        $member = $this->cachedMemberByProjectAndUser($user, $project);
 
         return $member !== null && $member->isLead();
     }
 
     private function isLeadOrMemberInProject(User $user, Project $project): bool
     {
-        $member = $this->projectMemberRepository->byProjectAndUserOrNull($project, $user);
+        $member = $this->cachedMemberByProjectAndUser($user, $project);
 
         return $member !== null && ($member->isLead() || $member->isMember());
+    }
+
+    private function cachedMemberByProjectAndUser(User $user, Project $project): ?ProjectMember
+    {
+        $cacheKey = sprintf('isLeadOrMemberInProject.%s.%s', $user->id, $project->id);
+
+        return Cache::remember($cacheKey, now()->addSecond(), function () use ($project, $user) {
+            return $member = $this->projectMemberRepository->byProjectAndUserOrNull($project, $user);
+        });
     }
 
     private function isInProject(User $user, Project $project): bool
