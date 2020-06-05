fbd54ac2c30c94538477bebb88cf400407b44b86
Albert221 <w.albert221@gmail.com>
2020-06-06 00:57:53 +0200

    Add section comments to routes file

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,22 @@ Auth::routes([
     'verify' => true,
 ]);
 
+// Dashboard
 Route::get('/', 'DashboardController')->name('dashboard');
 
+// Project
 Route::resource('/projects', 'Project\ProjectController');
 
+// Project import
 Route::get('/projects/{project}/import', 'Project\ImportController@show')->name('projects.import');
 Route::post('/projects/{project}/import', 'Project\ImportController@upload')->name('projects.import-upload');
 
+// Project export
 Route::get('/projects/{project}/export', 'Project\ExportController@show')->name('projects.export');
 Route::post('/projects/{project}/export/all', 'Project\ExportController@all')->name('projects.export-all');
 Route::post('/projects/{project}/export', 'Project\ExportController@language')->name('projects.export-language');
 
+// Project language
 Route::get('/projects/{project}/languages', 'Project\ProjectLanguageController@index')
     ->name('project-languages.index');
 Route::get('/projects/{project}/languages/create', 'Project\ProjectLanguageController@create')
@@ -37,21 +42,27 @@ Route::post('/projects/{project}/languages', 'Project\ProjectLanguageController@
 Route::delete('/projects/{project}/languages/{language_code}', 'Project\ProjectLanguageController@destroy')
     ->name('project-languages.destroy');
 
+// Project message
 Route::resource('/projects/{project}/messages', 'Project\MessageController')
     ->except(['show']);
 
+// Project message value
 Route::put('/projects/{project}/messages/{message}/{language_code}/{form?}', 'Project\MessageValueController@put')
     ->name('message-values.put');
 
+// Project member
 Route::resource('/projects/{project}/members', 'Project\ProjectMemberController', ['names' => 'project-members'])
     ->except(['show']);
 
+// Language
 Route::resource('/languages', 'LanguageController')
     ->except(['show']);
 
+// User
 Route::resource('/users', 'UserController')
     ->except(['show']);
 
+// Administration
 Route::get('/administration/statistics', 'AdministrationController@statistics')
     ->name('administration.statistics');
 Route::get('/administration/settings', 'AdministrationController@settings')->name('administration.settings');
@@ -59,11 +70,13 @@ Route::post('/administration/settings', 'AdministrationController@updateSettings
     ->name('administration.update-settings');
 Route::get('/administration/logs', 'AdministrationController@logs')->name('administration.logs');
 
+// Secret
 Route::get('/account/secrets', 'SecretController@index')->name('account-secrets.index');
 Route::get('/account/secrets/create', 'SecretController@create')->name('account-secrets.create');
 Route::post('/account/secrets', 'SecretController@store')->name('account-secrets.store');
 Route::delete('/account/secrets/{secret}', 'SecretController@revoke')->name('account-secrets.revoke');
 
+// Preferences
 Route::get('/account/preferences', 'AccountController@preferences')->name('account.preferences');
 Route::post('/account/preferences', 'AccountController@updatePreferences')->name('account.update-preferences');
 Route::get('/account/change-password', 'AccountController@changePassword')->name('account.change-password');
