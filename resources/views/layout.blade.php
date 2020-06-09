a2ae4b2b1da88885d68ed1ad78208f1352b67409
Albert221 <w.albert221@gmail.com>
2020-06-10 00:00:29 +0200

    Change default font to Roboto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
         </title>
 
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @stack('head')
     </head>
