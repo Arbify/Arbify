4ca5379d97dce781794907be284b897b4b6c8739
Albert221 <w.albert221@gmail.com>
2020-06-10 00:06:28 +0200

    Make sub layouts headers white

--- resources/views/administration/layout.blade.php
+++ resources/views/administration/layout.blade.php
@@ -1,22 +1,30 @@
 @extends('layout')
 
+@section('breadcrumbs', '')
+
 @section('content')
-    <div class="container">
-        <h2 class="mb-3">Administration</h2>
-        <ul class="nav nav-tabs mb-4">
-            <li class="nav-item">
-                <a href="{{ route('administration.statistics') }}"
-                   class="nav-link @if(request()->route()->getName() == 'administration.statistics') active @endif">Statistics</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('administration.settings') }}"
-                   class="nav-link @if(request()->route()->getName() == 'administration.settings') active @endif">Settings</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('administration.logs') }}"
-                   class="nav-link @if(request()->route()->getName() == 'administration.logs') active @endif">🚧 Logs</a>
-            </li>
-        </ul>
+    <div class="bg-white border-bottom pt-4 mb-4">
+        <div class="container">
+            <nav class="mb-4">
+                {{ Breadcrumbs::render() }}
+            </nav>
+
+            <h2 class="mb-3">Administration</h2>
+            <ul class="nav nav-tabs border-bottom-0">
+                <li class="nav-item">
+                    <a href="{{ route('administration.statistics') }}"
+                       class="nav-link @if(request()->route()->getName() == 'administration.statistics') active @endif">Statistics</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('administration.settings') }}"
+                       class="nav-link @if(request()->route()->getName() == 'administration.settings') active @endif">Settings</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('administration.logs') }}"
+                       class="nav-link @if(request()->route()->getName() == 'administration.logs') active @endif">🚧 Logs</a>
+                </li>
+            </ul>
+        </div>
     </div>
 
     @yield('administration-content')
--- resources/views/projects/layout.blade.php
+++ resources/views/projects/layout.blade.php
@@ -1,43 +1,51 @@
 @extends('layout')
 
+@section('breadcrumbs', '')
+
 @section('content')
-    <div class="container">
-        <div class="mb-3 d-flex align-items-center flex-column flex-sm-row">
-            <h2 class="mr-auto">{{ $project->name }}</h2>
-            <div class="mt-2 mt-sm-0">
-                <div class="btn-group mr-2">
-                    @can('update', $project)
-                        <a href="{{ route('projects.edit', $project) }}" class="btn btn-outline-secondary">Edit</a>
-                    @endcan
-                </div>
-                <div class="btn-group">
-                    @can('import', $project)
-                        <a href="{{ route('projects.import', $project) }}" class="btn btn-outline-primary">Import</a>
-                    @endcan
-                    <a href="{{ route('projects.export', $project) }}" class="btn btn-outline-primary">Export</a>
+    <div class="bg-white border-bottom pt-4 mb-4">
+        <div class="container">
+            <nav class="mb-4">
+                {{ Breadcrumbs::render() }}
+            </nav>
+
+            <div class="mb-3 d-flex align-items-center flex-column flex-sm-row">
+                <h2 class="mr-auto">{{ $project->name }}</h2>
+                <div class="mt-2 mt-sm-0">
+                    <div class="btn-group mr-2">
+                        @can('update', $project)
+                            <a href="{{ route('projects.edit', $project) }}" class="btn btn-outline-secondary">Edit</a>
+                        @endcan
+                    </div>
+                    <div class="btn-group">
+                        @can('import', $project)
+                            <a href="{{ route('projects.import', $project) }}" class="btn btn-outline-primary">Import</a>
+                        @endcan
+                        <a href="{{ route('projects.export', $project) }}" class="btn btn-outline-primary">Export</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <ul class="nav nav-tabs mb-4">
-            <li class="nav-item">
-                <a href="{{ route('projects.show', $project) }}"
-                   class="nav-link @if(request()->route()->getName() == 'projects.show') active @endif">Overview</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('messages.index', $project) }}"
-                   class="nav-link @if(request()->route()->getName() == 'messages.index') active @endif"><b>Messages</b></a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('project-languages.index', $project) }}"
-                   class="nav-link @if(request()->route()->getName() == 'project-languages.index') active @endif">Languages</a>
-            </li>
-            @can('view-any', [Arbify\Models\ProjectMember::class, $project])
+            <ul class="nav nav-tabs border-bottom-0">
                 <li class="nav-item">
-                    <a href="{{ route('project-members.index', $project) }}"
-                       class="nav-link @if(request()->route()->getName() == 'project-members.index') active @endif">Members</a>
+                    <a href="{{ route('projects.show', $project) }}"
+                       class="nav-link @if(request()->route()->getName() == 'projects.show') active @endif">Overview</a>
                 </li>
-            @endcan
-        </ul>
+                <li class="nav-item">
+                    <a href="{{ route('messages.index', $project) }}"
+                       class="nav-link @if(request()->route()->getName() == 'messages.index') active @endif"><b>Messages</b></a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('project-languages.index', $project) }}"
+                       class="nav-link @if(request()->route()->getName() == 'project-languages.index') active @endif">Languages</a>
+                </li>
+                @can('view-any', [Arbify\Models\ProjectMember::class, $project])
+                    <li class="nav-item">
+                        <a href="{{ route('project-members.index', $project) }}"
+                           class="nav-link @if(request()->route()->getName() == 'project-members.index') active @endif">Members</a>
+                    </li>
+                @endcan
+            </ul>
+        </div>
     </div>
 
     @yield('project-content')
