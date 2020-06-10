a3e66faf30f59e2a99983f0318f1a5453b189312
Albert221 <w.albert221@gmail.com>
2020-06-10 23:23:02 +0200

    Fix private projects shown for users

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -4,23 +4,14 @@ declare(strict_types=1);
 
 namespace Arbify\Repositories;
 
-use Arbify\Contracts\Repositories\MessageValueRepository;
 use Arbify\Contracts\Repositories\ProjectRepository as ProjectRepositoryContract;
-use Arbify\Models\Message;
 use Arbify\Models\Project;
 use Arbify\Models\User;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\JoinClause;
 
 class ProjectRepository implements ProjectRepositoryContract
 {
-    private MessageValueRepository $messageValueRepository;
-
-    public function __construct(MessageValueRepository $messageValueRepository)
-    {
-        $this->messageValueRepository = $messageValueRepository;
-    }
-
     public function byId(int $id): Project
     {
         return Project::findOrFail($id);
@@ -28,14 +19,14 @@ class ProjectRepository implements ProjectRepositoryContract
 
     public function visibleToUserPaginated(User $user): LengthAwarePaginator
     {
-        return Project::query()
-            ->where('visibility', '=', Project::VISIBILITY_PUBLIC)
-            ->orWhereExists(function (Builder $builder) use ($user) {
+        return Project::select('projects.*')
+            ->leftJoin('project_members', function (JoinClause $builder) use ($user) {
                 $builder
-                    ->from('projects')
-                    ->join('project_members', 'project_members.project_id', '=', 'projects.id')
+                    ->on('project_members.project_id', '=', 'projects.id')
                     ->where('project_members.user_id', '=', $user->id);
             })
+            ->where('visibility', '=', Project::VISIBILITY_PUBLIC)
+            ->orWhereNotNull('project_members.id')
             ->orderBy('name')
             ->paginate(30);
     }
